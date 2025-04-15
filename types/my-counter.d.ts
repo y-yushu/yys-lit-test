3a2d15e5acc4a5f0537138a7a2961eb7735e6fa7
杨 玉书 <yyshu@hisw.cn>
2025-04-15 18:02:53 +0800

    version: 1.0.12

---
+++ types/my-counter.d.ts
@@ -0,0 +1,8 @@
+import { LitElement } from "lit";
+export declare class MyCounter extends LitElement {
+    static styles: import("lit").CSSResult;
+    count: number;
+    increment(): void;
+    decrement(): void;
+    render(): import("lit").TemplateResult<1>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
+  plugins: [],
   build: {
     lib: {
       entry: "src/index.ts",
